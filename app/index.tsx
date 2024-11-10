6cfd7576d415530ec195898bc0b3e7473494f3ca
riki <aguilcasariki@gmail.com>
2024-11-10 02:22:28 -0500

    App ready

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,9 @@ import {
   FlatList,
   StyleSheet,
   Alert,
+  Image,
 } from "react-native";
+
 import auth from "@react-native-firebase/auth";
 import firestore from "@react-native-firebase/firestore";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
@@ -50,33 +52,54 @@ const Header = () => (
   </View>
 );
 
+// Asegúrate de que esta ruta sea correcta
+
 const ChatRoom = () => {
-  /* const messageRef = firestore().collection("messages");
-  const dummy = useRef(null);
-  const q = query(messageRef, orderBy("createdAt"), limit(25));
-  const [messages] = firestore(q, { idField: "id" });
+  const messageRef = firestore().collection("messages");
+  const [messages, setMessages] = useState([]);
   const [formValue, setFormValue] = useState("");
+  const flatListRef = useRef(null); // Crear una referencia para FlatList
+
+  // Fetch messages from Firestore
+  useEffect(() => {
+    const unsubscribe = messageRef
+      .orderBy("createdAt")
+      .limit(25)
+      .onSnapshot((snapshot) => {
+        const fetchedMessages = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setMessages(fetchedMessages);
+      });
+
+    return () => unsubscribe();
+  }, []);
 
   const sendMessage = async () => {
-    const { uid, photoURL } = auth.currentUser || {};
-    await addDoc(messageRef, {
+    if (!formValue.trim()) return; // Evita enviar mensajes vacíos
+
+    const { uid, photoURL } = auth().currentUser || {};
+    setFormValue(""); // Limpiar el campo de entrada
+
+    await messageRef.add({
       text: formValue,
       createdAt: new Date(),
       uid,
       photoURL,
     });
-    setFormValue("");
-    dummy.current?.scrollIntoView({ behavior: "smooth" });
   };
- */
+
   return (
     <View style={styles.chatRoom}>
-      <Text>CHAT ROOM</Text>
-      {/* <FlatList
+      <FlatList
+        onContentSizeChange={() =>
+          flatListRef.current.scrollToEnd({ animated: true })
+        }
+        ref={flatListRef} // Asignar la referencia aquí
         data={messages}
         renderItem={({ item }) => <ChatMessage message={item} />}
         keyExtractor={(item) => item.id}
-        inverted // To show the latest messages at the bottom
       />
       <View style={styles.inputContainer}>
         <TextInput
@@ -85,64 +108,74 @@ const ChatRoom = () => {
           placeholder="Type your message"
           style={styles.input}
         />
-        <Button title="Send" onPress={sendMessage} disabled={!formValue} />
-      </View> */}
+        <Button
+          title="Send"
+          onPress={sendMessage}
+          disabled={!formValue.trim()}
+        />
+      </View>
     </View>
   );
 };
 
 const ChatMessage = ({ message }) => {
-  const { text, uid } = message;
+  const { text, uid, photoURL } = message;
   const messageStyle =
-    uid === auth.currentUser?.uid ? styles.myMessage : styles.otherMessage;
+    uid === auth().currentUser?.uid ? styles.myMessage : styles.otherMessage;
 
   return (
     <View style={[styles.messageContainer, messageStyle]}>
-      <Text>{text}</Text>
+      <Image
+        source={{ uri: photoURL }}
+        style={styles.userPhoto}
+        resizeMode="cover"
+      />
+      <Text style={styles.messageText}>{text}</Text>
     </View>
   );
 };
 
 const SignIn = () => {
-  GoogleSignin.configure();
+  GoogleSignin.configure({
+    webClientId:
+      "468302392063-i1p31mlaijng1etoht94epcmq3mph2pn.apps.googleusercontent.com",
+  });
   async function onGoogleButtonPress() {
-    try {
-      // Check if your device supports Google Play
-      await GoogleSignin.hasPlayServices({
-        showPlayServicesUpdateDialog: true,
-      });
-      // Get the users ID token
-      const signInResult = await GoogleSignin.signIn();
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({
+      showPlayServicesUpdateDialog: true,
+    });
+    // Get the users ID token
+    const signInResult = await GoogleSignin.signIn();
+    console.log("signInResult", signInResult);
 
-      // Try the new style of google-sign in result, from v13+ of that module
-      idToken = signInResult.data?.idToken;
-      if (!idToken) {
-        // if you are using older versions of google-signin, try old style result
-        idToken = signInResult.idToken;
-      }
-      if (!idToken) {
-        throw new Error("No ID token found");
-      }
-
-      // Create a Google credential with the token
-      const googleCredential = auth.GoogleAuthProvider.credential(
-        signInResult.data.token
-      );
-
-      // Sign-in the user with the credential
-      return auth().signInWithCredential(googleCredential);
-    } catch (error) {
-      console.error(error);
+    // Try the new style of google-sign in result, from v13+ of that module
+    idToken = signInResult.data?.idToken;
+    if (!idToken) {
+      // if you are using older versions of google-signin, try old style result
+      idToken = signInResult.idToken;
     }
+    if (!idToken) {
+      throw new Error("No ID token found");
+    }
+    console.log("token", idToken);
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(
+      signInResult.data?.idToken || ""
+    );
+
+    return auth().signInWithCredential(googleCredential);
   }
   return (
     <View style={styles.signInContainer}>
       <GoogleSigninButton
         size={GoogleSigninButton.Size.Wide}
         color={GoogleSigninButton.Color.Dark}
-        onPress={() => {
-          onGoogleButtonPress();
-        }}
+        onPress={() =>
+          onGoogleButtonPress().then(() =>
+            console.log("Signed in with Google!")
+          )
+        }
       />
     </View>
   );
@@ -150,7 +183,7 @@ const SignIn = () => {
 
 const SignOut = () => {
   return (
-    auth.currentUser && (
+    auth().currentUser && (
       <Button
         title="Sign Out"
         onPress={() => auth().signOut()}
@@ -163,65 +196,97 @@ const SignOut = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f0f0f0",
+    backgroundColor: "#f5f5f5", // Light gray background
   },
   header: {
-    backgroundColor: "#6200ee",
+    backgroundColor: "#007AFF", // Blue header
     padding: 16,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
   },
   headerText: {
     color: "#fff",
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: "bold",
   },
   chatContainer: {
     flex: 1,
     paddingTop: StatusBar.currentHeight || 0,
+    backgroundColor: "#fff",
   },
   chatRoom: {
     flex: 1,
-    paddingHorizontal: 10,
-    paddingBottom: 10,
+    paddingHorizontal: 16,
+    paddingBottom: 16,
     backgroundColor: "#fff",
   },
   inputContainer: {
     flexDirection: "row",
     alignItems: "center",
-    borderTopWidth: StyleSheet.hairlineWidth,
-    borderTopColor: "#ccc",
+    borderTopWidth: 1,
+    borderTopColor: "#eee",
     paddingVertical: 10,
+    backgroundColor: "#fff",
   },
   input: {
     flex: 1,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderColor: "#ccc",
-    borderRadius: 5,
-    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 25,
+    paddingHorizontal: 16,
     marginRight: 10,
-    height: 40,
+    height: 50,
+    backgroundColor: "#fff",
+    color: "#333",
   },
   messageContainer: {
     marginVertical: 5,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    borderRadius: 5,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderRadius: 20,
+    gap: 10,
+  },
+  userPhoto: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  messageText: {
+    padding: 10,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "#ddd",
   },
   myMessage: {
-    backgroundColor: "#6200ee",
     alignSelf: "flex-end",
+
     color: "#fff",
+    textAlign: "right",
+    padding: 10,
+    borderRadius: 20,
+
+    flexDirection: "row-reverse",
   },
+
+  // Update the otherMessage style
   otherMessage: {
-    backgroundColor: "#e0e0e0",
     alignSelf: "flex-start",
+    backgroundColor: "#f0f0f0",
+    color: "#333",
+    textAlign: "left",
+    padding: 10,
+    borderRadius: 20,
+    maxWidth: "70%",
+    flexDirection: "row",
   },
   signInContainer: {
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
     flex: 1,
+    backgroundColor: "#fff",
   },
 });
